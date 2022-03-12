982cd6dbb09802a2db628da86e0011af80aa24f9
babak bigdeli <babakbigdeli@gmial.com>
2022-03-12 16:06:02 +0000

    entry point updated

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const reverseWords = require('./reverseWords');
 const digitize = require('./digitize');
 const decodeMorse = require('./decodeMorse');
 const lastDigit = require('./lastDigitOfaLargeNumber')
+const evaluatePokerHand = require('./evaluatePokerHand')
 
 
 module.exports = {
     reverseWords,
     digitize,
     decodeMorse,
-    lastDigit
+    lastDigit,
+    evaluatePokerHand
 
 };
